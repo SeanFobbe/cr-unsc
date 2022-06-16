60b814e4d1376d9b5009673c9f14d186fbe9b4ab
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-16 18:06:08 +0200

    add extract metalink function

---
+++ functions/f.extract_metalink.R
@@ -0,0 +1,46 @@
+
+#' For a given UN Security Council resolution, extract the relevant page in the UN Digital Library.
+
+#' @param resno The number of a UN Security Council resolution.
+#'
+#' @return The relative 
+
+
+extract_metalink <- function(resno){
+
+    query <-     paste0("https://digitallibrary.un.org/search?ln=en&as=1&m1=p&p1=S%2FRES%2F",
+                        resno,
+                        "(&f1=documentsymbol&op1=a&m2=a&p2=&f2=&op2=a&m3=a&p3=&f3=&dt=&d1d=&d1m=&d1y=&d2d=&d2m=&d2y=&rm=&ln=en&sf=&so=d&rg=50&c=United%20Nations%20Digital%20Library%20System&of=hb&fti=0&fti=0&fct__1=Resolutions%20and%20Decisions")
+
+    links <- f.linkextract(query)
+    record <- unique(grep("/record/[0-9]+$", links, value = TRUE))
+
+    if(length(record) == 0){
+        
+        query <-     paste0("https://digitallibrary.un.org/search?ln=en&as=1&m1=p&p1=S%2FRES%2F",
+                            resno,
+                            "+(&f1=documentsymbol&op1=a&m2=a&p2=&f2=&op2=a&m3=a&p3=&f3=&dt=&d1d=&d1m=&d1y=&d2d=&d2m=&d2y=&rm=&ln=en&sf=&so=d&rg=50&c=United%20Nations%20Digital%20Library%20System&of=hb&fti=0&fti=0&fct__1=Resolutions%20and%20Decisions")
+
+        links <- f.linkextract(query)
+        record <- unique(grep("/record/[0-9]+$", links, value = TRUE))
+        
+    }
+
+    record.absolute <- paste0("https://digitallibrary.un.org",
+                              record)
+    
+
+    if(length(record) == 0){
+
+        message(paste("No link acquired for", resno))
+        
+    }
+
+    Sys.sleep(runif(1, 1, 2))
+    
+    message(resno)
+
+    return(record.absolute)
+    
+    
+}
