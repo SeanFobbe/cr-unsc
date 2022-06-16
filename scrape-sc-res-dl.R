590d8323a836d547b370435da79e5c31c7ccc6e5
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-06-16 18:44:03 +0200

    update test file

--- scrape-sc-res-dl.R
+++ scrape-sc-res-dl.R
@@ -38,28 +38,23 @@ extract_metalink <- function(resno){
 
     if(length(record) == 0){
         
-    query <-     paste0("https://digitallibrary.un.org/search?ln=en&as=1&m1=p&p1=S%2FRES%2F",
-                        resno,
-                        "+(&f1=documentsymbol&op1=a&m2=a&p2=&f2=&op2=a&m3=a&p3=&f3=&dt=&d1d=&d1m=&d1y=&d2d=&d2m=&d2y=&rm=&ln=en&sf=&so=d&rg=50&c=United%20Nations%20Digital%20Library%20System&of=hb&fti=0&fti=0&fct__1=Resolutions%20and%20Decisions")
+        query <-     paste0("https://digitallibrary.un.org/search?ln=en&as=1&m1=p&p1=S%2FRES%2F",
+                            resno,
+                            "+(&f1=documentsymbol&op1=a&m2=a&p2=&f2=&op2=a&m3=a&p3=&f3=&dt=&d1d=&d1m=&d1y=&d2d=&d2m=&d2y=&rm=&ln=en&sf=&so=d&rg=50&c=United%20Nations%20Digital%20Library%20System&of=hb&fti=0&fti=0&fct__1=Resolutions%20and%20Decisions")
 
-    links <- f.linkextract(query)
-    record <- unique(grep("/record/[0-9]+$", links, value = TRUE))
+        links <- f.linkextract(query)
+        record <- unique(grep("/record/[0-9]+$", links, value = TRUE))
         
     }
 
     if(length(record) == 0){
 
-        message(paste("Empty Response for", resno))
+        message(paste("No link acquired for", resno))
         
-    ##     sleeptime <- runif(1, 300, 360)
-
-    ##     message(paste("Empty response, sleeping until", Sys.time() + sleeptime))
-        
-    ##     Sys.sleepSys.sleep()
-        
-     }
+    }
 
     Sys.sleep(runif(1, 1, 2))
+    
     message(resno)
 
     return(record)
@@ -78,7 +73,7 @@ library(data.table)
 
 res_nos_full <- 1:2636
 
-maintable <- fread("data/UNSC_Record-Pages_2022-06-16.csv")
+maintable <- fread("data/UNSC_Record-Pages_2636.csv")
 
 res_nos_work <- setdiff(res_nos_full, maintable$res_nos)
 
