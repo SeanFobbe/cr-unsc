9f82a474022f934d4cb1c13f837487596feb5e22
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-10-09 22:33:31 +0200

    add downoad table create fun

---
+++ functions/f.download_table_create.R
@@ -0,0 +1,39 @@
+
+
+#' @param record.table Data.table. A table of UNSC resolution numbers and Digital Library record pages.
+#' @param sleep Numeric. The number of seconds to sleep between requests.
+#' @param debug Logical. Whether to download only a subet of resolutions.
+#' @param debug.sample Integer. A vector of resolution numbers to download.
+
+
+
+
+f.download_table_create <- function(record.table,
+                                    sleep = runif(1, 1, 2),
+                                    debug = FALSE,
+                                    debug.sample = sample(2640, 50)){
+
+
+    if(debug = TRUE){
+
+        record.table <- record.table[sort(debug.sample)]
+
+    }
+
+
+    
+    metadata.list <- lapply(X = record.table[1:10]$url_record,
+                            FUN = f.record_metadata,                       
+                            sleep = sleep)
+    
+
+    metadata.dt <- rbindlist(metadata.list, fill = TRUE)
+
+
+    dt.final <- cbind(record.table, metadata.dt)
+
+    
+    return(dt.final)
+
+
+}
