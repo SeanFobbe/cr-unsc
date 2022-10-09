e1bd63f2f546d209958764284784745a40e5d739
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-10-09 22:33:53 +0200

    remove manual debugging

--- functions/f.download_table_create.R
+++ functions/f.download_table_create.R
@@ -22,7 +22,7 @@ f.download_table_create <- function(record.table,
 
 
     
-    metadata.list <- lapply(X = record.table[1:10]$url_record,
+    metadata.list <- lapply(X = record.table$url_record,
                             FUN = f.record_metadata,                       
                             sleep = sleep)
     
