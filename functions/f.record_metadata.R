9fe90bbd9ffe1d5cba0d61fd1f7ad9f67496d723
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-10-13 21:45:14 +0200

    add trycatch to meta data extraction fun

--- functions/f.record_metadata.R
+++ functions/f.record_metadata.R
@@ -21,96 +21,104 @@ f.record_metadata <- function(url,
 
         message(url)
         
+    }
+
+
+    tryCatch({
+        
+        
+        ## Read HTML
+        html <- rvest::read_html(url)
+        
+        ## Nodes
+        nodes <- rvest::html_nodes(html, "[class='metadata-row']")
+
+        ## Variable names
+        varname <- rvest::html_nodes(html, "[class='title col-xs-12 col-sm-3 col-md-2']") %>% html_text()
+        varname <- trimws(varname)
+        varname <- gsub(" ", "_", varname)
+        varname <- tolower(varname)
+
+        ## Variable content
+        content.nodes <- rvest::html_nodes(html, "[class='value col-xs-12 col-sm-9 col-md-10']")
+        content.text <- html_text2(content.nodes)
+        content.text <- trimws(content.text)
+
+        ## Draft
+        draft.pos <- grep("draft", varname, ignore.case = TRUE)
+
+        if(length(draft.pos) == 1){
+
+            url_record_draft <- html_nodes(content.nodes[[draft.pos]], "a") %>% html_attr("href")
+
+        }else if(length(draft.pos) == 0){
+
+            url_record_draft <- NA_character_
+
+        }else{
+
+            url_record_draft <- "Error: More than one draft record found!"
+
         }
 
-    
-    ## Read HTML
-    html <- rvest::read_html(url)
-    
-    ## Nodes
-    nodes <- rvest::html_nodes(html, "[class='metadata-row']")
-
-    ## Variable names
-    varname <- rvest::html_nodes(html, "[class='title col-xs-12 col-sm-3 col-md-2']") %>% html_text()
-    varname <- trimws(varname)
-    varname <- gsub(" ", "_", varname)
-    varname <- tolower(varname)
-
-    ## Variable content
-    content.nodes <- rvest::html_nodes(html, "[class='value col-xs-12 col-sm-9 col-md-10']")
-    content.text <- html_text2(content.nodes)
-    content.text <- trimws(content.text)
-
-    ## Draft
-    draft.pos <- grep("draft", varname, ignore.case = TRUE)
-
-    if(length(draft.pos) == 1){
-
-        url_record_draft <- html_nodes(content.nodes[[draft.pos]], "a") %>% html_attr("href")
-
-    }else if(length(draft.pos) == 0){
-
-        url_record_draft <- NA_character_
-
-    }else{
-
-        url_record_draft <- "Error: More than one draft record found!"
-
-    }
-
-    
-    ## Meeting Record
-    meeting.pos <- grep("meeting", varname, ignore.case = TRUE)
+        
+        ## Meeting Record
+        meeting.pos <- grep("meeting", varname, ignore.case = TRUE)
 
 
-    if(length(meeting.pos) == 1){
+        if(length(meeting.pos) == 1){
 
-        url_record_meeting <- html_nodes(content.nodes[[meeting.pos]], "a") %>% html_attr("href")
+            url_record_meeting <- html_nodes(content.nodes[[meeting.pos]], "a") %>% html_attr("href")
 
-    }else if(length(meeting.pos) == 0){
+        }else if(length(meeting.pos) == 0){
 
-        url_record_meeting <- NA_character_
+            url_record_meeting <- NA_character_
 
-    }else{
+        }else{
 
-        url_record_meeting <- "Error: More than one meeting record found!"
+            url_record_meeting <- "Error: More than one meeting record found!"
 
-    }
+        }
 
-    
-    ## Subjects
-    subjects <- rvest::html_nodes(html, "[class='metadata-row rs-list-row-inner']") %>% html_text()
-    subjects <- gsub("\n", "", subjects)
-    subjects <- trimws(subjects)
-    subjects <- gsub(" {2,}", " > ", subjects)
-    subjects <- paste0(subjects, collapse = "|")
+        
+        ## Subjects
+        subjects <- rvest::html_nodes(html, "[class='metadata-row rs-list-row-inner']") %>% html_text()
+        subjects <- gsub("\n", "", subjects)
+        subjects <- trimws(subjects)
+        subjects <- gsub(" {2,}", " > ", subjects)
+        subjects <- paste0(subjects, collapse = "|")
 
 
 
-    ## Create table
-    dt.meta <- data.table(varname, content.text)
-    dt.meta <- transpose(dt.meta, make.names = "varname")
-    
-    dt.meta <- cbind(dt.meta,
-                     subjects,
-                     url_record_draft,
-                     url_record_meeting)
+        ## Create table
+        dt.meta <- data.table(varname, content.text)
+        dt.meta <- transpose(dt.meta, make.names = "varname")
+        
+        dt.meta <- cbind(dt.meta,
+                         subjects,
+                         url_record_draft,
+                         url_record_meeting)
 
 
-    ## Doc ID
-    dt.meta$doc_id <- gsub("\\/|\\(", "_", dt.meta$symbol)
-    dt.meta$doc_id <- gsub("\\)| ", "", dt.meta$doc_id)
+        ## Doc ID
+        dt.meta$doc_id <- gsub("\\/|\\(", "_", dt.meta$symbol)
+        dt.meta$doc_id <- gsub("\\)| ", "", dt.meta$doc_id)
 
-    ## Acquire URLs
-    dt.pdf <- f.record_extract_url(html)
+        ## Acquire URLs
+        dt.pdf <- f.record_extract_url(html)
 
-    ## Finalize
-    dt.final <- cbind(dt.meta, dt.pdf)
+        ## Finalize
+        dt.final <- cbind(dt.meta, dt.pdf)
 
-    ## Sleep
-    Sys.sleep(sleep)
-    
-    return(dt.final)
+        ## Sleep
+        Sys.sleep(sleep)
+        
+        return(dt.final)
+
+    },
+    error = function(cond) {
+        return(NA)}
+    )
     
 }
 
