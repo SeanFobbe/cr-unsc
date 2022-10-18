6bc7a1d8373f9fcf206e8e1e586212b03b659a59
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-10-18 16:41:10 +0200

    generalize url extraction from record pages

---
+++ functions/f.record_url.R
@@ -0,0 +1,72 @@
+#' f.record_url
+#'
+#' Extract all URLs to the full texts for a given document from a UN Digital Library record page.
+#'
+#' @param html A UN Digital Library record page in HTML format.
+#'
+#' @return A data.table of all URLs to texts in each UN language.
+
+
+
+## sample link:  "/record/111952/files/S_RES_37%281947%29-ES.pdf"
+
+
+
+f.record_url <- function(x,
+                         prefix = ""){
+
+
+
+    ## Read HTML
+    html <- rvest::read_html(x)
+    
+    links <- rvest::html_nodes(html, "a") %>% html_attr('href')
+
+    pdf.relative <- unique(grep("/record/[0-9]+/files", links, value = TRUE))
+
+    pdf.absolute <- paste0("https://digitallibrary.un.org",
+                           pdf.relative)
+
+    ar <- f.grep.NA("AR\\.pdf", pdf.absolute)    
+    en <- f.grep.NA("EN\\.pdf", pdf.absolute)
+    es <- f.grep.NA("ES\\.pdf", pdf.absolute)
+    fr <- f.grep.NA("FR\\.pdf", pdf.absolute)
+    ru <- f.grep.NA("RU\\.pdf", pdf.absolute)
+    zh <- f.grep.NA("ZH\\.pdf", pdf.absolute)
+
+
+    dt.final <- data.table(ar,
+                           en,
+                           es,
+                           fr,
+                           ru,
+                           zh)
+
+    setnames(dt.final,
+             new = paste0(prefix, names(dt.final)))
+    
+    return(dt.final)
+    
+}
+
+
+
+
+#' f.grep.NA
+#'
+#' Search for string, return value. If no value is found, return NA. Arguments as with classic grep.
+
+
+f.grep.NA <- function(pattern, x){
+
+    value <- grep(pattern = pattern, x = x, value = TRUE)
+
+    if (length(value) == 0){
+
+        value <- NA_character_
+        
+    }
+
+    return(value)
+
+}
