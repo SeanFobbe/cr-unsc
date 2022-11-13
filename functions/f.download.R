0030a4d95448a3a9e27d3fb0ea883f46c4031c61
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-11-13 23:52:58 +0100

    rwrite download fun; add random order option; structure download todo as data frames

--- functions/f.download.R
+++ functions/f.download.R
@@ -11,6 +11,7 @@
 #' @param retries Positive Integer. Number of retries for entire download set.
 #' @param retry.sleep.min Positive Integer. Minimum number of seconds to randomly sleep between requests in retry mode.
 #' @param retry.sleep.max Positive Integer. Maximum number of seconds to randomly sleep between requests in retry mode.
+#' @param random.order Logical. Whether to download files in random order. Defaults to TRUE.
 #' @param timeout Positive Integer. Number of seconds for a download to timeout, even active ongoing ones.
 #' @param debug.toggle Logical. Whether to activate debugging mode.
 #' @param debug.files Positive Integer. The number of files to download during debugging mode. Default is 500.
@@ -26,76 +27,116 @@ f.download <- function(url,
                        retries = 3,
                        retry.sleep.min = 2,
                        retry.sleep.max = 5,
+                       random.order = TRUE,
                        timeout = 300,
                        debug.toggle = FALSE,
                        debug.files = 500){
+
+    ## Set timeout for downloads
+    options(timeout = timeout)
     
     ## Test for equality of length
     if(length(url) != length(filename)){
 
-        stop("Length of arguments url and filename is not equal.")
+        stop("Length of arguments 'url' and 'filename' is not equal.")
         
-    }
-
-    ## Set timeout for downloads
-    options(timeout = timeout)
-
-    ## [Debugging Mode] Reduce download scope
-    if (debug.toggle == TRUE){
-
-        sample.index <- sample(length(url), debug.files)
-
-        url <- url[sample.index]
-        filename <- filename[sample.index]
-
     }
 
     ## Create folder
     dir.create(dir, showWarnings = FALSE)
+    
+    ## Create Data Frame
+    df <- data.frame(url, filename)
 
+
+    ## [Debugging Mode] Reduce download scope
+    if (debug.toggle == TRUE){
+
+        sample.index <- sort(sample(nrow(df), debug.files))
+        df <- df[sample.index,]
+
+    }
+
+    
     ## Clean folder: Only files included in 'filename' may be present
+    
     files.all <- list.files(dir, full.names = TRUE)
-    delete <- setdiff(files.all, file.path(dir, filename))
+    delete <- setdiff(files.all, file.path(dir, df$filename))
     unlink(delete)
 
+
+    
     ## Exclude already downloaded files
+    
     files.present <- list.files(dir)
-    filename.todo <- setdiff(filename, files.present)
-    url.todo  <- url[filename %in% filename.todo]
+    filename.todo <- setdiff(df$filename, files.present)
+    df.todo  <- df[df$filename %in% filename.todo,]
+
+
     
     
     ## Download: First Try
-    for (i in sample(length(url.todo))){
-        tryCatch({download.file(url = url.todo[i],
-                                destfile = file.path(dir,
-                                                     filename.todo[i]))
-        },
-        error = function(cond) {
-            return(NA)}
-        )
+
+    if(random.order = TRUE){
         
-        Sys.sleep(runif(1, sleep.min, sleep.max))
+        download.order <- sample(nrow(df.todo))
+
+    }else{
+        
+        download.order <- 1:nrow(df.todo)
+        
     }
 
+
+    if(nrow(df.todo) > 0){
+        
+        for (i in download.order){
+            
+            tryCatch({download.file(url = df.todo$url[i],
+                                    destfile = file.path(dir,
+                                                         df.todo$filename[i]))
+            },
+            
+            error = function(cond) {
+                return(NA)}
+            )
+            
+            Sys.sleep(runif(1, sleep.min, sleep.max))
+        }
+
+    }
     
 
 
     ## Download: Retries
 
     for(i in 1:retries){
-
+        
         ## Missing files
+        
         files.present <- list.files(dir)
-        filename.missing <- setdiff(filename, files.present)
-        url.missing  <- url[filename %in% filename.missing]
+        filename.missing <- setdiff(df$filename, files.present)
+        df.missing  <- df[df$filename %in% filename.missing,]
 
-        if(length(filename.missing) > 0){
-            
-            for (i in 1:length(filename.missing)){
+        if(nrow(df.missing) > 0){
+
+
+            if(random.order = TRUE){
                 
-                tryCatch({download.file(url = url.missing[i],
+                download.order <- sample(nrow(df.missing))
+
+            }else{
+                
+                download.order <- 1:nrow(df.missing)
+                
+            }
+            
+            
+            for (i in download.order){
+                
+                tryCatch({download.file(url = df.missing$url[i],
                                         destfile = file.path(dir,
-                                                             filename.missing[i]))
+                                                             df.missing$filename[i]))
                 },
                 error = function(cond) {
                     return(NA)}
@@ -107,6 +148,10 @@ f.download <- function(url,
         
     }
 
+
+
+    ## Report Missing Files
+    
     if(length(filename.missing) > 0){
 
         warning(paste("Missing file:", filename.missing, collapse = "\n"))
@@ -114,10 +159,12 @@ f.download <- function(url,
     }
 
 
+    
     ## Final File Count
+    
     files.all <- list.files(dir, full.names = TRUE)
-
     message(paste("Downloaded", length(files.all), "of", length(filename), "files."))
+
 
     return(files.all)
     
