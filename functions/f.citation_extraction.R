35b27c5073fd1116d326270ef5bef7b58f5fc739
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-02-04 16:42:34 +0100

    add to citation extraction

--- functions/f.citation_extraction.R
+++ functions/f.citation_extraction.R
@@ -4,7 +4,36 @@
 f.citation_extraction <- function(text){
 
 
-    res <- stringi::stri_extract_all(text, regex = "[0-9]{1,4} \\([0-9]{4}\\)")
+    target <- stringi::stri_extract_all(text, regex = "[0-9]{1,4} \\([0-9]{4}\\)")
+
+    source <- paste0(dt.final$res_no, " (", dt.final$year, ")")
+    
+    bind <- mapply(cbind, source, target)
+
+    bind2 <- lapply(bind, as.data.table)
+
+    dt <- rbindlist(bind2)
 
 
+
+g  <- igraph::graph.data.frame(dt,
+                               directed = TRUE)
+
+
+ggraph(g,) + 
+    geom_edge_diagonal(colour = "grey")+
+    geom_node_point()+
+    geom_node_text(aes(label = name),
+                   size = 2,
+                   repel = TRUE)+
+    theme_void()
+    
+    
+
+res[[1]]
+
+out[[1]]
+    
+    
+str(dt.final)
     }
