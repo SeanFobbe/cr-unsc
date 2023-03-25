2efb594e4f7ecdf6d1b6a043c323237e1db08de3
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-03-25 02:43:50 +0100

    add language models for tesseract

--- requirements-tesseract.sh
+++ requirements-tesseract.sh
@@ -1,17 +1,17 @@
-# Install System Dependencies
+## Install System Dependencies
 
 apt-get install automake ca-certificates g++ git libtool libleptonica-dev make pkg-config
 
 apt-get install --no-install-recommends asciidoc docbook-xsl xsltproc
 
 
-# Clone Tesseract Repository
+## Clone Tesseract Repository
 
 git clone  https://github.com/tesseract-ocr/tesseract.git --branch 5.3.0 --single-branch
 
 
 
-# Compile from Source
+## Compile from Source
 
 cd tesseract
 ./autogen.sh
@@ -19,4 +19,30 @@ cd tesseract
 make
 sudo make install
 sudo ldconfig
+
+
+
+## Download Language Models
+
+# English
+wget -O ${TESSDATA_PREFIX}/eng.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/eng.traineddata
+
+# Chinese
+wget -O ${TESSDATA_PREFIX}/chi_sim.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/chi_sim.traineddata
+
+# French
+wget -O ${TESSDATA_PREFIX}/fra.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/fra.traineddata
+
+# Spanish
+wget -O ${TESSDATA_PREFIX}/spa.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/spa.traineddata
+
+# Arabic
+wget -O ${TESSDATA_PREFIX}/ara.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/ara.traineddata
+
+# Russian
+wget -O ${TESSDATA_PREFIX}/rus.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/rus.traineddata
+
+# German
+wget -O ${TESSDATA_PREFIX}/deu.traineddata https://github.com/tesseract-ocr/tessdata_fast/blob/main/deu.traineddata
+
 
