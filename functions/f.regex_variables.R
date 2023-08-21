a332e9cabef54a5f273326d5e8bf10ca5f22e750
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-08-21 03:33:27 +0200

    rewrite regex extraction; add many terms

--- functions/f.regex_variables.R
+++ functions/f.regex_variables.R
@@ -11,9 +11,35 @@
 f.regex_variables <- function(text){
 
 
-    chapter7 <- grepl("Chapter VII", text)
+
+    human_rights <- stringi::stri_detect_regex(text, "\\bhuman *rights\\b",
+                                               case_insensitive = TRUE) * 1
     
-    dt.return <- data.table(chapter7)
+    chapter6 <- stringi::stri_detect_regex(text, "\\bChapter *VI\\b") * 1
+    chapter7 <- stringi::stri_detect_regex(text, "\\bChapter *VII\\b") * 1
+    chapter8 <- stringi::stri_detect_regex(text, "\\bChapter *VIII\\b") * 1
+    
+    peace_threat <- stringi::stri_detect_regex(text, "\\bthreat *to *the *peace\\b",
+                                               case_insensitive = TRUE) * 1
+
+    peace_breach <- stringi::stri_detect_regex(text, "\\bbreach *of *the *peace\\b",
+                                               case_insensitive = TRUE) * 1
+
+    aggression <- stringi::stri_detect_regex(text, "\\baggression\\b",
+                                             case_insensitive = TRUE) * 1
+
+    self_defence <- stringi::stri_detect_regex(text, "\\bself-defen[cs]e\\b",
+                                               case_insensitive = TRUE) * 1
+
+    
+    dt.return <- data.table(human_rights,
+                            chapter6,
+                            chapter7,
+                            chapter8,
+                            peace_threat,
+                            peace_breach,
+                            aggression,
+                            self_defence)
 
 
 
