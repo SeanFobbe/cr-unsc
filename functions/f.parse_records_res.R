5d30239c6dbe8b7fde55889284f9ab0dc5e9db7e
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-08-22 00:14:39 +0200

    add voting metadata parsing fun

--- functions/f.parse_records_res.R
+++ functions/f.parse_records_res.R
@@ -46,3 +46,26 @@ f.parse_records_url <- function(x,
     return(dt.final)
 
 }
+
+
+
+#' Parses UN Digital Library HTML record files and returns a data.table with relevant voting metadata for all records.
+
+#' @param x String. A vector of paths to UN Digital Library HTML voting record files.
+
+#' @return Data.table. A table of all relevant metadata. Includes the UNSC resolution number as unique key.
+
+
+f.parse_records_voting <- function(x){
+
+    list <- lapply(x, f.record_metadata_voting)
+
+    dt <- rbindlist(list, fill = TRUE)
+
+    res_no <- as.integer(tools::file_path_sans_ext(basename(x)))
+
+    dt.final <- cbind(res_no, dt)[order(res_no)]
+
+    return(dt.final)
+
+}
