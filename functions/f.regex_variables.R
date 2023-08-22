309721f98c3a4703b9140b895a46cab712f13d1b
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-08-22 21:08:32 +0200

    switch dummy vars to logical

--- functions/f.regex_variables.R
+++ functions/f.regex_variables.R
@@ -13,23 +13,23 @@ f.regex_variables <- function(text){
 
 
     human_rights <- stringi::stri_detect_regex(text, "\\bhuman *rights\\b",
-                                               case_insensitive = TRUE) * 1
+                                               case_insensitive = TRUE)
     
-    chapter6 <- stringi::stri_detect_regex(text, "\\bChapter *VI\\b") * 1
-    chapter7 <- stringi::stri_detect_regex(text, "\\bChapter *VII\\b") * 1
-    chapter8 <- stringi::stri_detect_regex(text, "\\bChapter *VIII\\b") * 1
+    chapter6 <- stringi::stri_detect_regex(text, "\\bChapter *VI\\b")
+    chapter7 <- stringi::stri_detect_regex(text, "\\bChapter *VII\\b")
+    chapter8 <- stringi::stri_detect_regex(text, "\\bChapter *VIII\\b")
     
     peace_threat <- stringi::stri_detect_regex(text, "\\bthreat *to *the *peace\\b",
-                                               case_insensitive = TRUE) * 1
+                                               case_insensitive = TRUE)
 
     peace_breach <- stringi::stri_detect_regex(text, "\\bbreach *of *the *peace\\b",
-                                               case_insensitive = TRUE) * 1
+                                               case_insensitive = TRUE)
 
     aggression <- stringi::stri_detect_regex(text, "\\baggression\\b",
-                                             case_insensitive = TRUE) * 1
+                                             case_insensitive = TRUE)
 
     self_defence <- stringi::stri_detect_regex(text, "\\bself-defen[cs]e\\b",
-                                               case_insensitive = TRUE) * 1
+                                               case_insensitive = TRUE)
 
     
     dt.return <- data.table(human_rights,
