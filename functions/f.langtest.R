0fd7ccaed785bb72535e2e4c1e28b8bda61a6746
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2023-11-05 18:43:50 +0100

    remove non-western languages from language purity test

--- functions/f.langtest.R
+++ functions/f.langtest.R
@@ -13,33 +13,22 @@
 f.langtest <- function(dt.final){
 
 
-    lang.profiles <- TC_byte_profiles[names(TC_byte_profiles) %in% c("arabic-iso8859_6",
-                                                                     "arabic-windows1256",
-                                                                     "english",
+    lang.profiles <- TC_byte_profiles[names(TC_byte_profiles) %in% c("english",
                                                                      "french",
-                                                                     "spanish",
-                                                                     "chinese-big5",
-                                                                     "chinese-gb2312",
-                                                                     "russian-iso8859_5",
-                                                                     "russian-koi8_r",
-                                                                     "russian-windows1251")]
+                                                                     "spanish")]
     
 
-    ar_langtest <- textcat(dt.final$text_ar, p = lang.profiles)
+
     en_langtest <- textcat(dt.final$text, p = lang.profiles)
     es_langtest <- textcat(dt.final$text_es, p = lang.profiles)
     fr_langtest <- textcat(dt.final$text_fr, p = lang.profiles)
-    ru_langtest <- textcat(dt.final$text_ru, p = lang.profiles)
-    zh_langtest <- textcat(dt.final$text_zh, p = lang.profiles)
+
     
 
     dt.final <- data.table(res_no = dt.final$res_no,
-                           ar_langtest,
                            en_langtest,
                            es_langtest,
-                           fr_langtest,
-                           ru_langtest,
-                           zh_langtest)
+                           fr_langtest)
 
     return(dt.final)
 
